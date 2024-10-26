b13356f9364fd5a8555055c4dcc4ff2329547975
thisismemukul <w41k3r.12@gmail.com>
2024-10-26 15:19:54 +0530

    health controller

---
+++ src/main/java/com/user/management/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.user.management.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthController(){
+    return ResponseEntity.ok("Health OK");
+    }
+}
