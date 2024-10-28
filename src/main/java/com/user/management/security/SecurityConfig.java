1aba3d9a6f3ef08fbf5e5534f2d9369ec6438581
thisismemukul <w41k3r.12@gmail.com>
2024-10-28 19:41:14 +0530

    UserDetailsService Bean

--- src/main/java/com/user/management/security/SecurityConfig.java
+++ src/main/java/com/user/management/security/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,13 +20,36 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-/*       .requestMatchers("/public/**").permitAll()
-          .requestMatchers("/admin").denyAll()
-           http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+/*      .requestMatchers("/public/**").permitAll()
+        .requestMatchers("/admin").denyAll()
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
  */
         http.csrf(AbstractHttpConfigurer::disable);
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager =
+                new InMemoryUserDetailsManager();
+        if (!manager.userExists("mukul")) {
+            manager.createUser(
+                    User.withUsername("mukul")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
