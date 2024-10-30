1753a2460aee57827fd173aff259df41ff4aecb5
thisismemukul <w41k3r.12@gmail.com>
2024-10-30 21:39:33 +0530

    EnableMethodSecurity

--- src/main/java/com/user/management/controller/AdminController.java
+++ src/main/java/com/user/management/controller/AdminController.java
@@ -5,12 +5,14 @@ import com.user.management.request.dto.UserDTO;
 import com.user.management.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     private final UserService userService;
--- src/main/java/com/user/management/security/SecurityConfig.java
+++ src/main/java/com/user/management/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.user.management.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true,
+        proxyTargetClass = true)
 public class SecurityConfig {
 
     @Bean
