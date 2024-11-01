9555b617034ecc833ee93d57df1ddda4713ba727
thisismemukul <w41k3r.12@gmail.com>
2024-11-01 19:20:33 +0530

    get username

--- src/main/java/com/user/management/controller/AuthController.java
+++ src/main/java/com/user/management/controller/AuthController.java
@@ -172,4 +172,8 @@ public class AuthController {
 
         return ResponseEntity.ok().body(response);
     }
+    @GetMapping("/username")
+    public String currentUserName(@AuthenticationPrincipal UserDetails userDetails) {
+        return (userDetails != null) ? userDetails.getUsername() : "";
+    }
 }
